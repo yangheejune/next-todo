515f0139423d17f1e86fdd76f9ca67caf77e01be
지란지교 <jiransecurity@jiransecurity.onmicrosoft.com>
2022-11-09 17:01:45 +0900

    05. 투두리스트 만들기

---
+++ components/TodoList.tsx
@@ -0,0 +1,205 @@
+import React, { useMemo, useCallback } from "react";
+import styled from "styled-components";
+import palette from "../styles/palette";
+import { TodoType } from "../types/todo";
+import TrashCanIcon from "../public/statics/svg/trash_can.svg";
+import CheckMarkIcon from "../public/statics/svg/Vector.svg";
+
+interface IProps {
+  todos: TodoType[];
+}
+
+const Container = styled.div`
+  width: 100%;
+
+  .todo-num {
+    margin-left: 12px;
+  }
+
+  .todo-list-header {
+    padding: 12px;
+    position: relative;
+    border-bottom: 1px solid ${palette.gray};
+
+    .todo-list-last-todo {
+      font-size: 14px;
+      margin: 0 0 8px;
+      span {
+        margin-left: 12px;
+      }
+    }
+
+    .todo-list-header-colors {
+      display: flex;
+      .todo-list-header-color-num {
+        display: flex;
+        margin-right: 8px;
+        p {
+          font-size: 14px;
+          line-height: 16px;
+          margin: 0;
+          margin-left: 6px;
+        }
+        .todo-list-header-round-color {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+        }
+      }
+    }
+  }
+
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
+  }
+
+  .todo-list {
+    .todo-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 52px;
+      border-bottom: 1px solid ${palette.gray};
+
+      .todo-left-side {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .todo-color-block {
+          width: 12px;
+          height: 100%;
+        }
+        .checked-todo-text {
+          color: ${palette.gray};
+          text-decoration: line-through;
+        }
+        .todo-text {
+          margin-left: 12px;
+          font-size: 16px;
+        }
+      }
+      .todo-right-side {
+        display: flex;
+        margin-right: 12px;
+        svg {
+          &:first-child {
+            margin-right: 16px;
+          }
+        }
+        .todo-trash-can {
+          width: 16px;
+          path {
+            fill: ${palette.deep_red};
+          }
+        }
+        .todo-check-mark {
+          fill: ${palette.deep_green};
+        }
+        .todo-button {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          border: 1px solid ${palette.gray};
+          background-color: transparent;
+          outline: none;
+        }
+      }
+    }
+  }
+`;
+
+//* 객체의 문자열 인덱스 사용을 위한 타입
+type ObjectIndexType = {
+  [key: string]: number | undefined;
+};
+
+const TodoList: React.FC<IProps> = ({ todos }) => {
+  //* 색깔 객체 구하기 2
+  const todoColorNums = useMemo(() => {
+    const colors: ObjectIndexType = {};
+    todos.forEach((todo) => {
+      const value = colors[todo.color];
+      if (!value) {
+        //* 존재하지 않는 key라면
+        colors[`${todo.color}`] = 1;
+      } else {
+        //* 존재하는 key라면
+        colors[`${todo.color}`] = value + 1;
+      }
+    });
+    return colors;
+  }, [todos]);
+
+  console.log(todoColorNums);
+
+  return (
+    <Container>
+      <div className="todo-list-header">
+        <p className="todo-list-last-todo">
+          남은 TODO<span>{todos.length}개</span>
+        </p>
+        <div className="todo-list-header-colors">
+          {Object.keys(todoColorNums).map((color, index) => (
+            <div className="todo-list-header-color-num" key={index}>
+              <div className={`todo-list-header-round-color bg-${color}`} />
+              <p>{todoColorNums[color]}개</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            <div className="todo-left-side">
+              <div className={`todo-color-block bg-${todo.color}`} />
+              <p
+                className={`todo-text ${
+                  todo.checked ? "checked-todo-text" : ""
+                }`}>
+                {" "}
+                {todo.text}
+              </p>
+            </div>
+            <div className="todo-right-side">
+              {todo.checked && (
+                <>
+                  <TrashCanIcon className="todo-trash-can" onClick={() => {}} />
+                  <CheckMarkIcon
+                    className="todo-check-mark"
+                    onClick={() => {}}
+                  />
+                </>
+              )}
+              {!todo.checked && (
+                <button
+                  type="button"
+                  className="todo-button"
+                  onClick={() => {}}
+                />
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default TodoList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
 import { NextPage } from "next";
 import styled from "styled-components";
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
 
-const Container = styled.div`
-  padding: 20px;
-`;
+const todos: TodoType[] = [
+  {
+    id: 1,
+    text: "SaniTOX EP 서버 데이터베이스 정리",
+    color: "red",
+    checked: false,
+  },
+  { id: 2, text: "SaniTOX EP UI 설명", color: "orange", checked: false },
+  { id: 3, text: "음료수 가지고 오기", color: "yellow", checked: false },
+  { id: 4, text: "TodoList 클론코딩 하기", color: "green", checked: true },
+  { id: 5, text: "설거지 하기", color: "blue", checked: false },
+  { id: 6, text: "게임 정리하기", color: "navy", checked: false },
+];
 
-const index: NextPage = () => {
-  return (
-    <Container>
-      <h1>hello Styled-components</h1>
-      <h2>hello Styled-components</h2>
-      <p>hello Styled-components</p>
-      <ul>
-        <li>hello Styled-components</li>
-      </ul>
-      <a>hello Styled-components</a>
-      <span>hello Styled-components</span>
-    </Container>
-  );
+const app: NextPage = () => {
+  return <TodoList todos={todos} />;
 };
 
-export default index;
+export default app;
